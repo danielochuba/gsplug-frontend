043f7a3072cc77b934b3fe2dc399a6a0a690ad95
Daniel Ochuba <danielochuba78@gmail.com>
2024-08-06 11:30:21 +0100

    fix output directory

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FiMenu } from "react-icons/fi";
+import { VscChromeClose } from "react-icons/vsc";
 import UNN from '../assets/images/unn.jpeg';
 import ESUT from '../assets/images/esut.jpeg';
 import IMT from '../assets/images/imt.png';
@@ -23,26 +25,62 @@ import CONSULT from '../assets/images/consultance.jpeg';
 import POSTUTME from '../assets/images/postutme.jpeg';
 
 const Homepage = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+        document.body.style.overflow = isMenuOpen ? 'auto' : 'hidden';
+    };
     return (
-        <div className="min-h-screen bg-gray-100">
+        <div className={`min-h-screen bg-gray-100 ${isMenuOpen ? 'overflow-hidden ' : ''}`}>
             {/* Header */}
-            <header className="bg-blue-600 text-white p-4">
+            <header className="bg-blue-600 text-white p-4 w-full fixed">
                 <div className="container mx-auto flex justify-between items-center">
                     <h1 className="text-2xl font-bold">GS-Plug</h1>
-                    <nav>
-                        <ul className="flex space-x-4">
-                            <li><a href="#home" className="hover:underline">Home</a></li>
-                            <li><a href="#schools" className="hover:underline">Schools</a></li>
-                            <li><a href="#services" className="hover:underline">Services</a></li>
-                            <li><a href="#consultants" className="hover:underline">Consultants</a></li>
-                            <li><a href="#contact" className="hover:underline">Contact</a></li>
-                        </ul>
+                    <nav className=''>
+                        <div className="md:hidden">
+                            <button onClick={toggleMenu}>
+                                {isMenuOpen ? (
+                                    <></>
+                                ) : (
+                                    <FiMenu className='text-2xl'/>
+                                )}
+                            </button>
+                        </div>
+                        <div className={`md:flex space-x-4 ${isMenuOpen ? 'block ' : 'hidden'} absolute top-0 right-0 top- bg-sky-700 w-1/2 `}>
+                            <button onClick={toggleMenu}>
+                                {isMenuOpen ? (
+                                    <VscChromeClose className='absolute right-3 text-2xl' />
+                                ) : (
+                                    <></>
+                                )}
+                            </button>
+                            <ul className={`flex flex-col gap-6 text-2xl mt-10 py-10`}>
+                                <li><a href="#home" onClick={toggleMenu} className="block hover:underline p-2">Home</a></li>
+                                <li><a href="#schools" onClick={toggleMenu} className="block hover:underline p-2">Schools</a></li>
+                                <li><a href="#services" onClick={toggleMenu} className="block hover:underline p-2">Services</a></li>
+                                <li><a href="#consultants" onClick={toggleMenu} className="block hover:underline p-2">Consultants</a></li>
+                                <li><a href="#contact" onClick={toggleMenu} className="block hover:underline p-2">Contact</a></li>
+                            </ul>
+                        </div>
+
+                        {/* Header */}
+                        {/* <nav>
+                            <ul className={`flex space-x-4 ${isMenuOpen ? 'hidden' : 'block'} `}>
+                                <li><a href="#home" className="hover:underline">Home</a></li>
+                                <li><a href="#schools" className="hover:underline">Schools</a></li>
+                                <li><a href="#services" className="hover:underline">Services</a></li>
+                                <li><a href="#consultants" className="hover:underline">Consultants</a></li>
+                                <li><a href="#contact" className="hover:underline">Contact</a></li>
+                            </ul>
+                        </nav> */}
+
                     </nav>
                 </div>
             </header>
 
             {/* Hero Section */}
-            <section id="home" className="bg-blue-500 text-white text-center py-20">
+            <section id="home" className="bg-blue-500 text-white text-center pt-40 pb-20">
                 <div className="container mx-auto">
                     <h2 className="text-4xl font-bold mb-4">Welcome to GS-Plug Info Board</h2>
                     <p className="text-lg mb-8">Find all the information you need about your school of interest</p>
@@ -128,59 +166,59 @@ const Homepage = () => {
             <section id="services" className="bg-gray-100 py-20">
                 <div className="container mx-auto">
                     <h2 className="text-3xl font-bold text-center mb-12">Our Services</h2>
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 sm:mx-5">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mx-5">
                         {/* Example of Service Cards */}
                         <div className="bg-white p-6 rounded-lg shadow-md justify-center text-center">
-                            <img src={SCRATCH} alt="Scratch card" className='m-auto'/>
+                            <img src={SCRATCH} alt="Scratch card" className='m-auto' />
                             <h3 className="text-xl font-bold mb-2">WAEC Scratch Cards</h3>
                             <p>Purchase your WAEC scratch cards here.</p>
                         </div>
                         <div className="bg-white p-6 rounded-lg shadow-md justify-center text-center">
-                            <img src={UPLOAD} alt="Post-O-levels" className='m-auto'/>
+                            <img src={UPLOAD} alt="Post-O-levels" className='m-auto' />
                             <h3 className="text-xl font-bold mb-2">O-Level Upload Service</h3>
                             <p>Upload your O-Level results securely.</p>
                         </div>
                         <div className="bg-white p-6 rounded-lg shadow-md justify-center text-center">
-                            <img src={JAMB} alt="Upload" className='m-auto'/>
+                            <img src={JAMB} alt="Upload" className='m-auto' />
                             <h3 className="text-xl font-bold mb-2">JAMB / O-Level results</h3>
                             <p>Check and print your JAMB and O-Level results here.</p>
                         </div>
                         <div className="bg-white p-6 rounded-lg shadow-md justify-center text-center">
-                            <img src={ADMISSION} alt="Admission" className='m-auto'/>
+                            <img src={ADMISSION} alt="Admission" className='m-auto' />
                             <h3 className="text-xl font-bold mb-2">Admission Assistance</h3>
                             <p>Get help with your school admission process.</p>
                         </div>
                         <div className="bg-white p-6 rounded-lg shadow-md justify-center text-center">
-                            <img src={PAST} alt="Past questions" className='m-auto'/>
+                            <img src={PAST} alt="Past questions" className='m-auto' />
                             <h3 className="text-xl font-bold mb-2">Post-UTME Past Questions</h3>
                             <p>Get past questions for your school's post-UTME.</p>
                         </div>
 
                         <div className="bg-white p-6 rounded-lg shadow-md justify-center text-center">
-                            <img src={FORM} alt='Form' className='m-auto'/>
+                            <img src={FORM} alt='Form' className='m-auto' />
                             <h3 className="text-xl font-bold mb-2">Form Registeration</h3>
                             <p>Purchase and Register Forms for your school</p>
                         </div>
 
                         <div className="bg-white p-6 rounded-lg shadow-md justify-center text-center">
-                            <img src={CONSULT} alt="Consultation" className='m-auto'/>
+                            <img src={CONSULT} alt="Consultation" className='m-auto' />
                             <h3 className="text-xl font-bold mb-2">School Consultation</h3>
                             <p>Get information on available Schools that aligns with your needs, with your pressing question answered.</p>
                         </div>
                         <div className="bg-white p-6 rounded-lg shadow-md justify-center text-center">
-                            <img src={PAYMENT} alt="Payment" className='m-auto'/>
+                            <img src={PAYMENT} alt="Payment" className='m-auto' />
                             <h3 className="text-xl font-bold mb-2">School Fees Payment</h3>
                             <p>Pay your school fees online.</p>
                         </div>
                         <div className="bg-white p-6 rounded-lg shadow-md justify-center text-center">
-                            <img src={CHAT} alt="Chat" className='m-auto'/>
+                            <img src={CHAT} alt="Chat" className='m-auto' />
                             <h3 className="text-xl font-bold mb-2">Quick Chat</h3>
                             <p>Chat with our consultants for quick assistance.</p>
                         </div>
                     </div>
                 </div>
             </section>
-{/* 
+            {/* 
             Consultants Section
             <section id="consultants" className="py-20">
                 <div className="container mx-auto">
